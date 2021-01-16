b9eb1f37121c968277d50089db719499f953e25b
michalkoziara <mkoziara12@gmail.com>
2021-01-16 21:02:32 +0100

    Initial commit

---
+++ src/test/java/com/kozmapps/cryptolive/CryptoLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kozmapps.cryptolive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
