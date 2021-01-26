adc98f93e5efae106d8cd7a036aefdb3342d7995
michalkoziara <mkoziara12@gmail.com>
2021-01-26 03:06:53 +0100

    Change parameters in the price related endpoints

--- src/main/java/com/kozmapps/cryptolive/price/PriceController.java
+++ src/main/java/com/kozmapps/cryptolive/price/PriceController.java
@@ -3,10 +3,7 @@ package com.kozmapps.cryptolive.price;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,29 +19,38 @@ public class PriceController {
     PriceService priceService;
 
     @GetMapping("/minute")
-    List<Price> getMinutePrices(@RequestBody PriceRequest priceRequest) {
+    List<Price> getMinutePrices(
+            @RequestParam String coinFromSymbol,
+            @RequestParam String coinToSymbol,
+            @RequestParam int limit) {
         return priceService.getMinutePrices(
-                priceRequest.getCoinFromSymbol(),
-                priceRequest.getCoinToSymbol(),
-                priceRequest.getLimit()
+                coinFromSymbol,
+                coinToSymbol,
+                limit
         );
     }
 
     @GetMapping("/hourly")
-    List<Price> getHourlyPrices(@RequestBody PriceRequest priceRequest) {
+    List<Price> getHourlyPrices(
+            @RequestParam String coinFromSymbol,
+            @RequestParam String coinToSymbol,
+            @RequestParam int limit) {
         return priceService.getHourlyPrices(
-                priceRequest.getCoinFromSymbol(),
-                priceRequest.getCoinToSymbol(),
-                priceRequest.getLimit()
+                coinFromSymbol,
+                coinToSymbol,
+                limit
         );
     }
 
     @GetMapping("/daily")
-    List<Price> getDailyPrices(@RequestBody PriceRequest priceRequest) {
+    List<Price> getDailyPrices(
+            @RequestParam String coinFromSymbol,
+            @RequestParam String coinToSymbol,
+            @RequestParam int limit) {
         return priceService.getDailyPrices(
-                priceRequest.getCoinFromSymbol(),
-                priceRequest.getCoinToSymbol(),
-                priceRequest.getLimit()
+                coinFromSymbol,
+                coinToSymbol,
+                limit
         );
     }
 }
